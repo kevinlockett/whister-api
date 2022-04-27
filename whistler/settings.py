7acf705b48a86d5c3111f1dd132401171f7ce4af
Kevin Lockett <chlockett@msn.com>
2022-04-27 14:44:38 -0500

    deploy to Heroku

--- whistler/settings.py
+++ whistler/settings.py
@@ -154,7 +154,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = 'media/'
 
