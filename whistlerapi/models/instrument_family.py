0ea74af4f32e131fac4f93cf7bc3c66366281514
Kevin Lockett <chlockett@msn.com>
2022-04-29 15:36:29 -0500

    make it better

--- whistlerapi/models/instrument_family.py
+++ whistlerapi/models/instrument_family.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class InstrumentFamily(models.Model):
-    family = models.CharField(max_length=50)
+    family = models.CharField(max_length=100)
 
     class Meta:
         verbose_name_plural = 'InstrumentFamilies'
--- whistlerapi/models/role.py
+++ whistlerapi/models/role.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Role(models.Model):
-    name = models.CharField(max_length=505)
+    name = models.CharField(max_length=50)
 
     class Meta:
         verbose_name_plural = 'roles'
