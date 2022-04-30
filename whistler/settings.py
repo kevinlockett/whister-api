a2b31e8972a768adcbec371dc4f72e65df85ff1e
Kevin Lockett <chlockett@msn.com>
2022-04-30 17:15:15 -0500

    make it better

--- whistler/settings.py
+++ whistler/settings.py
@@ -73,7 +73,7 @@ SWAGGER_SETTINGS = {
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:3000',
     'http://127.0.0.1:3000',
-    'https://whistler-client.herokuapp.com/'
+    'https://whistler-client.herokuapp.com'
 )
 
 MIDDLEWARE = [
